824b52cad967f6c5994619f327bfa5ed46344fd9
Prabal Goyal <prabalgoyal55@gmail.com>
2023-09-17 12:00:23 +0530

    second Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products:[
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 12,
+            description: 'high quality pant',
+        },
+        {
+            name: 'Nike Linen Pant',
+            slug: 'nike-linen-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 150,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 15,
+            description: 'high quality pant',
+        },
+    ],
+};
+
+export default data;
