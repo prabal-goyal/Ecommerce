048ddbd0a600e1542985b1158af6dea5a4f1d254
Prabal Goyal <prabalgoyal55@gmail.com>
2023-09-30 12:25:34 +0530

    addtocart func

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -15,7 +15,7 @@ function Product(props) {
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>{product.price}</Card.Text>
+        <Card.Text>${product.price}</Card.Text>
         <Button>Add to Cart</Button>
       </Card.Body>
     </Card>
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 // import data from '../data';
 
@@ -53,9 +55,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>LOADING...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
